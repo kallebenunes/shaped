c61c4ab068ee481e1194361f0c9ad7dfce269b78
Kallebe Nunes <kallebenm@gmail.com>
2022-11-10 21:48:51 -0300

    feat: create authentication page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Home from './pages/Home'
+import AuthenticationPage from './pages/AuthenticationPages'
 
 function App() {
   const [isLogged, setIsLogged] = useState(true)
-
 
   return isLogged 
   ? (
     <Home/>
   )
   : (
-    <h2>Authentication pages</h2>
+    <AuthenticationPage/>
   )
 }
 
---
+++ src/pages/AuthenticationPages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from './style'
+const AuthenticationPage = () => {
+    return (
+        <Container>
+            <h1>Authentication Page</h1>
+        </Container>
+    )
+}
+export default AuthenticationPage
